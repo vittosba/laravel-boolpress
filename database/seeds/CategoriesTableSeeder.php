8f9adc3a1db9a0866d0cfbe0a1e6697ecb6d877f
vittosba <vitsbardella@gmail.com>
2022-02-03 17:51:10 +0100

    php categories seeder

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['HTML', 'CSS', 'JavaScript', 'PHP'];
+
+        foreach ($categories as $category) {
+            $new_category = new Category();
+
+            $new_category->name = $category;
+            $new_category->slug = Str::slug($new_category->name, '-');
+
+            $new_category->save();
+        }
+    }
+}
