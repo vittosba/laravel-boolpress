f85c2d091271a02b36aed6646ca6b85ff1893e6e
vittosba <vitsbardella@gmail.com>
2022-02-14 19:01:46 +0100

    add mail sender

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,9 +5,14 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\SendWelcomeEmail;
+
 class HomeController extends Controller
 {
     public function index() {
+        Mail::to(Auth::user()->email)->send(new SendWelcomeEmail(Auth::user()->name));
         return view('admin.home');
     }
 }
---
+++ resources/views/mails/welcome.blade.php
@@ -0,0 +1,3 @@
+<h1>Welcome, {{ $name }}</h1>
+
+<p>Helle there, welcome to our site!!</p>
